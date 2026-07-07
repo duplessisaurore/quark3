c54d3598f4aebca3017f62b0a3b6a4cc82cc1086
stellaurora <jean1398reborn@gmail.com>
2026-07-07 12:04:09 +1000

    feat: cargo fmt & clippy

--- crates/boson_lowerer/src/errors.rs
+++ crates/boson_lowerer/src/errors.rs
@@ -1,8 +1,7 @@
+//! All errors that can occur during the `Boson3`
+//! lowering pass
+
 use std::fmt::Display;
-
-/// All errors that can occur during the `Boson3`
-/// lowering pass
-
 
 /// All possible error kinds that can occurr
 /// during the lowering process
@@ -13,7 +12,7 @@ pub enum LoweringErrorKind {
     // Invalid argument to a directive
     InvalidArgument {
         expected: String,
-        got: String
+        got: String,
     },
 
     // Invalid number of fields named
@@ -21,8 +20,8 @@ pub enum LoweringErrorKind {
     InvalidNamedFieldsAmount {
         object_name: String,
         fields_expected: u64,
-        fields_got: u64
-    }
+        fields_got: u64,
+    },
 }
 
 /// Located version of `LoweringErrorKind`
@@ -30,7 +29,7 @@ pub struct LoweringError {
     kind: LoweringErrorKind,
 
     // Line in the input source file.
-    line: usize
+    line: usize,
 }
 
 impl LoweringErrorKind {
@@ -48,13 +47,23 @@ impl Display for LoweringErrorKind {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
             Self::UnclosedConstruct => {
-                write!(f, "An unclosed construct was found, make sure all `if`/`loop`/`try`s are closed!")
-            },
+                write!(
+                    f,
+                    "An unclosed construct was found, make sure all `if`/`loop`/`try`s are closed!"
+                )
+            }
             Self::InvalidArgument { expected, got } => {
                 write!(f, "expected `{expected}`, got `{got}`")
-            },
-            Self::InvalidNamedFieldsAmount { object_name, fields_expected, fields_got } => {
-                write!(f, "The object `{object_name}` expected `{fields_expected}` field names, got `{fields_got}` field names")
+            }
+            Self::InvalidNamedFieldsAmount {
+                object_name,
+                fields_expected,
+                fields_got,
+            } => {
+                write!(
+                    f,
+                    "The object `{object_name}` expected `{fields_expected}` field names, got `{fields_got}` field names"
+                )
             }
         }
     }
